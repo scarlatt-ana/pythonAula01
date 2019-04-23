b74fa06a964b2fc8e1545576838b313292e23a8b
Aula01python <scarlattaparecida@gmail.com>
2019-04-23 11:48:34 -0300

    python - lista, numbers, nomes

---
+++ Aula01.py
@@ -0,0 +1,138 @@
+#name = "Scarlat"
+
+#print(name[1:5])
+#print (len(name))
+#print (name.find('i'))
+#print(name.replace('Scarlat', 'Aparecida'))
+# PRINT (NAME.FIND (i)---> O QUE ESTIVER DENTRO DOS PARENTES SERA A PROCURA)
+
+#course =  'Estação Hack do Facebook'
+#print ('Hack' in course)
+#PARA SABER SE É FALSE E TRUE
+
+#CONDIÇÃO
+#summer = False
+#winter = False
+
+#if summer:
+# : ABRI O IF (VERDADEIRO)
+    #print ('use óculos de sol')
+    #print ('vá a praia')
+    #print ('use protetor')
+    #SE CONTINUAR NO MESMO LIMITE DO PRINT APLICA MAIS INFORMAÇOES
+
+#elif  winter:
+    #print ('Não esquece o casaco filho')
+
+
+#else:
+# : ABRI O ELSE (FALSO) - SENDO ABERTO O ELSE DO COMEÇO DA LINHA ABRIRÁ OUTRA INFORMAÇÃO    
+    #print('não use óculos, durma.')
+
+#green_bank = True 
+#DITA SE SÃO O MESMO RESULTADO, MESMO VALOR
+#criminal = True
+
+#if green_bank and criminal:
+    #AND NOT (NEGAÇÃO) OU OR (OU)
+    #print ('vai pra ferias')
+
+#While
+
+
+#LISTAS
+#numbers = [1, 4, 6, 6, 9, 345, 101]
+#numbers.append(20)
+#numbers.append(21)
+#numbers.insert(0, 99)
+#numbers.remove (345)
+#numbers.pop ()
+
+#numbers.clear () -- CUIDADO COM ESTE METODO, ELE APAGA TUDO  
+#numbers.index (9)
+#print(numbers.count(6))
+#print (numbers.reverse())
+
+
+#PARA CRIAR LISTA NOVA
+#numbers = [1, 4, 6, 6, 9, 345, 101]
+#nova_Lista = []
+
+#PARA NÃO REPETIR O VALOR OU NOME  (IN OU NOT IN)
+#for number in numbers:
+    #if number not in nova_Lista:
+     #   nova_Lista.append (number)
+#print(nova_Lista)
+
+
+#TUPLA OU TUPLE (nÃO CONSEGUE APAGAR MODIFICAR -  NÃO CONSEGUE MEXER (SO CONSEGUE VER A ORDEM))
+#cursos = ('Ingles', 'Portugues', 'Matematica')
+
+#DICIONARIOS - SALVA O ITEM QUE TEM DOIS VALORES (EX= NOME/HAYANE)
+
+#pessoa = {
+    #'name': 'Hayane',
+    #'age': 22,
+    #'adress': "área de preservação florestal",
+    #'skills': ['python', 'c', 'cobol', 'pascal', 'pearl', 'birlll']
+#}
+
+#print(pessoa['name'])
+#print(pessoa['address'])
+
+
+#  ATIVIDADE EM CASA #
+#lista = [1, 2, 3, 8, 10, 45, 12]
+#FAÇA UM CODIGO, ONDE É POSSIVEL IDENTIFICAR SE O ALUNO PASSOU NA FACULDADE OU NÃO. A MEDIA É 6, E ELE SÓ PODE TER 2 FALTAS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
